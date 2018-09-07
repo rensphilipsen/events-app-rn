e90f3aa1820c92ce9a09ab31ac63d8fa9b277bda
Rens Philipsen <philipsen.rens@gmail.com>
2018-09-07 17:22:14 +0200

    Added attending users

--- app/components/PeopleAttending/PeopleAttending.js
+++ app/components/PeopleAttending/PeopleAttending.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import {View} from "react-native";
+import {Image, ScrollView} from "react-native";
 import styles from './styles';
 
 const PeopleAttending = (props) => {
     return (
-        <View style={styles.view}>
-
-        </View>
+        <ScrollView style={styles.view} horizontal={true}>
+            <Image style={styles.person} source={require('../../../assets/user1.jpg')}/>
+            <Image style={styles.person} source={require('../../../assets/user2.jpg')}/>
+            <Image style={styles.person} source={require('../../../assets/user3.jpg')}/>
+            <Image style={styles.person} source={require('../../../assets/user4.jpg')}/>
+            <Image style={styles.person} source={require('../../../assets/user5.jpg')}/>
+            <Image style={styles.person} source={require('../../../assets/user6.jpg')}/>
+        </ScrollView>
     );
 };
 
--- app/components/PeopleAttending/styles.js
+++ app/components/PeopleAttending/styles.js
@@ -2,9 +2,17 @@ import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
     view: {
-        height: 80,
         width: '100%',
         backgroundColor: '#5e5e82',
-        marginBottom: 10
+        marginBottom: 10,
+        flex: 1,
+        flexDirection: 'row'
+    },
+    person: {
+        width: 65,
+        height: 65,
+        borderRadius: 35,
+        marginHorizontal: 7,
+        marginVertical: 15
     }
 });
