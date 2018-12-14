5a6e09d35f3b1334728f2e3b8a1ccbdd2df732e8
Rens Philipsen <philipsen.rens@gmail.com>
2018-12-14 14:44:00 +0100

    Fix date error

--- app/screens/Chat.js
+++ app/screens/Chat.js
@@ -1,45 +1,46 @@
 import React, {PureComponent} from 'react';
 import {GiftedChat} from 'react-native-gifted-chat'
+import moment from "moment";
 
 class Chat extends PureComponent {
-    state = {
-        messages: [],
-    }
+	state = {
+		messages: [],
+	}
 
-    componentWillMount() {
-        this.setState({
-            messages: [
-                {
-                    _id: 1,
-                    text: 'Hello developer',
-                    createdAt: new Date(),
-                    user: {
-                        _id: 2,
-                        name: 'React Native',
-                        avatar: 'https://placeimg.com/140/140/any',
-                    },
-                },
-            ],
-        })
-    }
+	componentWillMount() {
+		this.setState({
+			messages: [
+				{
+					_id: 1,
+					text: 'Hello developer',
+					createdAt: moment('2018-12-12 00:00:00').format('Y-M-D HH:mm'),
+					user: {
+						_id: 2,
+						name: 'React Native',
+						avatar: 'https://placeimg.com/140/140/any',
+					},
+				},
+			],
+		})
+	}
 
-    onSend(messages = []) {
-        this.setState(previousState => ({
-            messages: GiftedChat.append(previousState.messages, messages),
-        }))
-    }
+	onSend(messages = []) {
+		this.setState(previousState => ({
+			messages: GiftedChat.append(previousState.messages, messages),
+		}))
+	}
 
-    render() {
-        return (
-            <GiftedChat
-                messages={this.state.messages}
-                onSend={messages => this.onSend(messages)}
-                user={{
-                    _id: 1,
-                }}
-            />
-        )
-    }
+	render() {
+		return (
+			<GiftedChat
+				messages={this.state.messages}
+				onSend={messages => this.onSend(messages)}
+				user={{
+					_id: 1,
+				}}
+			/>
+		)
+	}
 }
 
 export default (Chat);
